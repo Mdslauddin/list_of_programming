5a892472c0445cd1f1db447464eca656db328eb4
MD SLAUDDIN <88940044+Mdslauddin@users.noreply.github.com>
2022-04-26 08:36:50 +0530

    Add files via upload

---
+++ Ruby/part-01.rb
@@ -0,0 +1,85 @@
+
+=begin
+
+what is Ruby?
+server side scripting interpreted 
+extension .rb
+atom text editor 
+atom runner
+=end
+
+=begin
+puts "slauddin " # new line print 
+print "Arzoo"
+print " is cool"
+
+
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+=end
+
+
+# variable
+
+=begin
+character_name="john"
+character_age="35"
+
+puts ( "There once was a man named " +character_name)
+
+puts " he was "+ character_age +"years old."
+
+character_name="Tom"
+puts "He really likes the name George" 
+
+puts "byt didn't like begin 70."
+
+
+=end
+
+
+# Data Types
+name="Mike"
+occupation="programmer"
+age=75
+Gpa=3.25
+ismale=true
+istall=false
+flaws=nil
+
+# work with string
+
+=begin 
+ puts "Giraffe\"Academy"
+ 
+ phrase = "Giraffe Academy"
+ puts phrase.upcase()
+
+puts phrase.downcase()
+=end
+
+
+=begin
+phrase = " Giraffe Academy"
+puts phrase.strip()
+puts phrase.length()
+
+puts phrase.include? "Academy"
+puts phrase[1]
+puts phrase[0,3]
+
+puts phrase.index("G")
+puts "programming".upcase()
+
+=end 
+# Ruby string method doc 
+
+
+
+
+
+
---
+++ Ruby/part-02.rb
@@ -0,0 +1,39 @@
+#ruby 2.5.3 
+=begin
+puts 5.86
+puts 5+9
+puts 5-9
+puts 5*9
+puts 5/9
+puts 2**3
+puts 10%3
+=end
+
+
+
+num=20
+puts num
+
+puts ("my fav num " + num.to_s)
+
+num= -20 # 20.487
+puts num.abs()
+
+num=20.687
+puts num.round()
+
+num = 20.1
+puts num.ceil()
+
+num = 20.9
+puts num.floor()
+
+puts Math.sqrt(36)
+puts Math.log(1)
+
+=begin
+puts 1+7
+puts 1.0+7
+puts 10/7
+puts 10/7.0
+=end
